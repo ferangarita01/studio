edace55723315f5d02c25982c29494f0a3ef2e0e
ElFerlo (ElFerloTV) <ferangaritam@gmail.com>
2025-08-09 02:24:23 +0000

    Permite que los buscadores te indexen

---
+++ src/app/robots.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from 'next'
+ 
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://wastewise-hdbhk.web.app';
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: `${baseUrl}/sitemap.xml`,
+  }
+}
